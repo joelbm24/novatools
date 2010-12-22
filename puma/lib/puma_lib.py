259f617fd1ba375a292a7275906d2094f12614f7
Joel <jmoore.bavc@gmail.com>
2010-12-21 17:11:56 -0800

    added some more stuff

--- puma/lib/puma_lib.py
+++ puma/lib/puma_lib.py
@@ -1,24 +1,31 @@
 import yaml, boto, os
-from boto.ec2.connection import EC2Connection
-
+from boto.ec2.regioninfo import RegionInfo
 
 class Puma:
     def __init__(self):
-        self.puma_dir = os.environ["HOME"] + "/puma"
+        self.puma_dir = os.environ["HOME"] + "/.puma"
     def parse_config(self):
         config_file = file(self.puma_dir + "/config.yml", "r")
         self.config = yaml.load(config_file)
-        return self.config
 
     def connect(self):
         #TODO add some error handling
-       self.conn = EC2Connection(self.config["access_id"], self.config["access_secret"])
-       self.conn = EC2Connection()
+        self.conn = boto.connect_ec2(aws_access_key_id=self.config["access_id"],
+                                aws_secret_access_key=self.config["access_secret"],
+                                is_secure=False,
+                                region=RegionInfo(None, 'nova', "10.255.24.10"),
+                                port=8773,
+                                path='/services/Cloud')
 
     def image_list(self):
-        self.images = self.conn.get_all_images
+        self.images = self.conn.get_all_images()
 
     def run_instance(self):
-        #TODO loop through the list images and find the matching image
+        #TODO loop through the list images and find a matching user specified image
         image = self.images[0]
-        image.run()
+        #image.run()
+
+puma = Puma()
+puma.parse_config()
+puma.connect()
+print puma.image_list()
