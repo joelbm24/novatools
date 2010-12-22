31c753de700abe8049da576b809a418ad54418e0
Jake Dahn <admin@jakedahn.com>
2010-12-22 00:24:54 -0600

    adding shenanigans

--- puma/lib/puma.py
+++ puma/lib/puma.py
@@ -5,14 +5,12 @@ from boto.ec2.regioninfo import RegionInfo
 
 class Puma:
     def __init__(self):
-        self.puma_dir = os.environ["HOME"] + "/.puma"
-    def config(self):
-        config_file = file(self.puma_dir + "/config.yml", "r")
-        return yaml.load(config_file)
+        config_file = file( os.environ["HOME"] + "/.puma" + "/config.yml", "r")
+        self.config = yaml.load(config_file) 
 
     def connect(self):
         #TODO add some error handling
-        return boto.connect_ec2(aws_access_key_id=self.config["access_id"],
+        return boto.ec2.connection.EC2Connection(aws_access_key_id=self.config["access_id"],
                                 aws_secret_access_key=self.config["access_secret"],
                                 is_secure=False,
                                 region=RegionInfo(None, 'nova', "10.255.24.10"),
